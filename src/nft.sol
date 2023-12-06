c84d77cef4bdcf0b05e059a141edc9ada91f61a5
setouchi-h <setouchi.kazuki@gmail.com>
2023-12-06 22:27:23 +0900

    second commit

---
+++ src/nft.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC721A} from "erc721a/contracts/ERC721A.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC6551Registry} from "../src/ERC6551Registry.sol";
+
+// contract Root is ERC721A, Ownable {}
